9d7321f4b24ccf1bf822c87af0cd79258ab0892f
m.qabbani96 <mohammad.qabany@fastlink.com.jo>
2023-08-02 16:22:10 +0300

    adding profile active

--- src/main/java/com/education/EducationApplication.java
+++ src/main/java/com/education/EducationApplication.java
@@ -1,8 +1,15 @@
 package com.education;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.education.entity.Student;
+import com.education.repository.StudentRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,9 +20,33 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 public class EducationApplication {
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EducationApplication.class, args);
+		
+		
 	}
 
+	@PostConstruct
+	public void creationFirst() {
+		Student std =  studentRepository.findByUserName("UserForRegister");
+		if(std != null) {
+			Student s = new Student();
+			
+			s.setFullName("Mohammad");
+			s.setUserName("UserForRegister");
+			s.setMobile("0796945841");
+			s.setEmail("qabba@gmail.com");
+			s.setAddress("Amman");
+			s.setPassword(bCryptPasswordEncoder.encode("Testing@123$"));
+			studentRepository.save(s);
+		}
+		
+	}
 	
 }
